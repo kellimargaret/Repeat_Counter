0326956590aabd53fed88fd374f4bc33a5bbe8d4
Kelli <kelli.sommerdyke@gmail.com>
2015-08-14 15:41:21 -0700

    Add HTML to counterresults.html.twig

--- app/app.php
+++ app/app.php
@@ -15,14 +15,9 @@
 
         //Matched results page
         $app->get('/results', function() use ($app) {
-            $word = $_GET['word'];
-            $string = $_GET['string'];
             $new_count = new RepeatCounter;
-            $count = $new_count->countRepeats($word, $string);
-
-            $twig_results = array($count, $word, $string);
-
-            return $app['twig']->render('conterresults.html.twig', array('results' => $twig_results));
+            $count = $new_count->countRepeats($_GET['word'], $_GET['string']);
+            return $app['twig']->render('counterresults.html.twig', array('results' => $count));
         });
 
     return $app;
