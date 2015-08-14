7ef8be17f0e69d4c80728471c772093219d66cdd
Kelli <kelli.sommerdyke@gmail.com>
2015-08-14 11:47:16 -0700

    Add lowercasing to both variables before passing through the function

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,6 +6,10 @@
         {
             $match = 0;
 
+            //Convert all inputs to lowercase before comparing
+            $word = strtolower($word);
+            $string = strtolower($string);
+
             if ($word == $string) {
                 $match = $match + 1;
             } else {
