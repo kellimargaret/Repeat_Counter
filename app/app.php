bcfb80061facb310b2c7af386fb9b8bc1430be82
Kelli <kelli.sommerdyke@gmail.com>
2015-08-14 15:23:03 -0700

    Add twig results page

--- app/app.php
+++ app/app.php
@@ -13,17 +13,17 @@
         return $app['twig']->render('counterform.html.twig');
     });
 
-    //Matched results page
-    $app->get('/results', function() use ($app) {
-        $word = $_GET['word'];
-        $string = $_GET['string'];
-        $new_count = new RepeatCounter;
-        $count = $new_count->countRepeats($word, $string);
+        //Matched results page
+        $app->get('/results', function() use ($app) {
+            $word = $_GET['word'];
+            $string = $_GET['string'];
+            $new_count = new RepeatCounter;
+            $count = $new_count->countRepeats($word, $string);
 
-        $twig_results = array($count, $word, $string);
+            $twig_results = array($count, $word, $string);
 
-        return $app['twig']->render('conterresults.html.twig', array('results' => $twig_results));
-    });
+            return $app['twig']->render('conterresults.html.twig', array('results' => $twig_results));
+        });
 
     return $app;
 
